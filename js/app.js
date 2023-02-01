a9f86d3a19f9c1a01a34d24faaebb372e70d5628
marypaduano <maria.paduano@gmail.com>
2023-02-01 18:09:54 +0100

    fix input element

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 console.log('work')
+
+const PRICE_KM = 0.21
+console.log('PRICE_KM')
+const discount = 0
+
+
+let button = document.getElementById('printTicket')
+button.addEventListener('click', function () {
+    let inputKm = document.getElementById('km')
+    console.log(inputKm.value)
+
+    let ageElementSelect = document.getElementById('age')
+    console.log(ageElementSelect.value)
+}
+
+
+
+
+)
+
+
+
